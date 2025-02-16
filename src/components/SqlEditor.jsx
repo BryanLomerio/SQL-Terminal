d3ca45fb71b0498bc005d83f8160b74bcf18b4f1
anino secret <bryanlomerioaninogithub@gmail.com>
2025-02-16 10:02:28 +0800

    feat: add prism javaScript

--- src/components/SqlEditor.jsx
+++ src/components/SqlEditor.jsx
@@ -1,18 +1,40 @@
-import React from 'react';
-import Editor from 'react-simple-code-editor';
 import Prism from 'prismjs';
 import 'prismjs/components/prism-sql';
-import 'prismjs/themes/prism.css';
+import 'prismjs/components/prism-javascript'; 
+import 'prismjs/themes/prism-tomorrow.css';
 
-const SqlEditor = ({ query, setQuery }) => {
+import React, { useMemo } from 'react';
+import Editor from 'react-simple-code-editor';
+
+if (Prism.languages.sql) {
+  Prism.languages.insertBefore('sql', 'keyword', {
+    'table-name': {
+      pattern: /((?:FROM|JOIN|INTO|UPDATE)\s+)(`?\w+`?)/i,
+      lookbehind: true,
+      alias: 'table-name'
+    },
+    'column-name': {
+      pattern: /(\.)\w+/,
+      lookbehind: true,
+      alias: 'column-name'
+    }
+  });
+}
+
+const CodeEditor = ({ query, setQuery, language = 'sql' }) => {
+  const highlightCode = useMemo(() => (code) => {
+    const grammar = Prism.languages[language] || Prism.languages.sql;
+    return Prism.highlight(code, grammar, language);
+  }, [language]);
+
   return (
     <Editor
       value={query}
       onValueChange={setQuery}
-      highlight={(code) => Prism.highlight(code, Prism.languages.sql, 'sql')}
+      highlight={highlightCode}
       padding={10}
       style={{
-        fontFamily: '"Fira code", "Fira Mono", monospace',
+        fontFamily: '"Fira Code", "Fira Mono", monospace',
         fontSize: 16,
         backgroundColor: '#1e1e1e',
         color: '#eee',
@@ -23,4 +45,4 @@ const SqlEditor = ({ query, setQuery }) => {
   );
 };
 
-export default SqlEditor;
+export default CodeEditor;
